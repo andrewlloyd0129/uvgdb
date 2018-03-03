60fa9f75805bf09a678e5a5d35c23d3c9a4e378f
andrewlloyd0129 <andrewlloyd0129@gmail.com>
2018-03-03 11:53:25 -0700

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'pages/home'
+
+  root to: "pages#home"
+
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
