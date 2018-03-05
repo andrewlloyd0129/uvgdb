cca5a63673de6e12ee3eb5da5e5eac0d9d331456
andrewlloyd0129 <andrewlloyd0129@gmail.com>
2018-03-05 13:00:34 -0700

    updated readme to do list

--- README.md
+++ README.md
@@ -2,12 +2,19 @@ TODO:
 
 *CRUD*
 - On games show page, if game was released in past x years, add month to year
+- CRUD for platforms
 - CRUD for Studio'
 - CRUD for Characters
 
 
 
+*USERS*
+- sign in/ sign out pages
+- admin user
+
+*Search Bar*
+- Drop down menu for what you are searching
+
 *Other Crap*
 - add gritter and make them show up (currently if you cannot create a new game it fails silently)
 
-*Testing Git hub right here*
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20180303191928) do
-
+ActiveRecord::Schema.define(version: 20180303202012) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "games", force: :cascade do |t|
+    t.string "title"
+    t.date "release"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -32,14 +38,6 @@ ActiveRecord::Schema.define(version: 20180303191928) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-
-  create_table "games", force: :cascade do |t|
-    t.string "title"
-    t.date "release"
-    t.text "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-
   end
 
 end
