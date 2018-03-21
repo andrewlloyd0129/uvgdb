91313e459fd36f4d1f1c7b082b7d95af990d7aa0
Dawnfable <DuskDawnfable@gmail.com>
2018-03-21 13:47:12 -0600

    fixed permissions for admin only access

--- app/controllers/platforms_controller.rb
+++ app/controllers/platforms_controller.rb
@@ -1,5 +1,6 @@
 class PlatformsController < ApplicationController
     before_action :set_platform, only: [:show, :edit, :update, :destroy]
+    access all: [:index, :show], user: {except: [:destroy, :new, :create, :update, :edit]}, admin: :all
 
   def index
     q_param = params[:q]
