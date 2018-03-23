b7c556d9e18a214facb40e75350ea349c00e16b9
andrewlloyd0129 <andrewlloyd0129@gmail.com>
2018-03-23 13:58:12 -0600

    updated gamplat and gamble with validations

--- app/models/gamplat.rb
+++ app/models/gamplat.rb
@@ -1,2 +1,3 @@
 class Gamplat < ApplicationRecord
+  validates_presence_of :platform_id, :game_id
 end
--- app/models/gample.rb
+++ app/models/gample.rb
@@ -1,2 +1,3 @@
 class Gample < ApplicationRecord
+  validates_presence_of :people_id, :game_id
 end
