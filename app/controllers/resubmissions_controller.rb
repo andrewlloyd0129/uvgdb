db1ad2dd440bbe13968f99c0cbdc007f692e1814
andrewlloyd0129 <andrewlloyd0129@gmail.com>
2018-04-10 14:34:10 -0600

    started the wiring of resubmissions.  went for a git hub party so i can get an admin dash

--- app/controllers/resubmissions_controller.rb
+++ app/controllers/resubmissions_controller.rb
@@ -1,2 +1,62 @@
 class ResubmissionsController < ApplicationController
+  before_action :set_thing, only: [:show, :edit, :update, :destroy]
+  access all: [:index, :show], user: {except: [:destroy, :new, :create, :update, :edit]}, admin: :all
+  
+  def index
+    @resubmissions = Resubmission.all
+  end
+
+  def new
+    @resubmission = Resubmission.new
+  end
+
+  def create
+    @resubmission = Resubmission.new(games_params)
+    searchable
+   if @resubmission.save
+      redirect_to games_path
+    else
+      render :new
+    end
+
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @resubmission.update(games_params)
+      searchable
+      @resubmission.save
+      redirect_to @resubmission
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @resubmission.destroy
+      redirect_to games_path, notice: 'Your post was edited successfully'
+    else
+      render :show, notice: 'penis'
+    end
+  end
+
+  private
+
+  def resubmission_params
+    params.require(:resubmission).permit(:title, :description, :email)
+  end
+
+  def set_thing
+    @resubmission = Resubmission.find(params[:id])
+  end
+  
+  def searchable
+    @resubmission.searchable = @resubmission.title + @resubmission.release.to_s + @resubmission.description
+  end
+
 end
